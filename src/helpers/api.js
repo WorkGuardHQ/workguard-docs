094fe0faf322494c3abd891cf408af449dd00dd7
Noura ElBrashy <noura.elbrashy@gmail.com>
2026-06-26 00:42:00 +0300

    feat: add SSO login and protected routes for documentation

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const publicApi = axios.create({
 //   baseURL: "https://YOUR-BACKEND-URL/api",
 //مؤقتا لحين رفع الباك
-  baseURL: `${window.location.origin.replace(':5173', ':5000')}/api`,
+ baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
